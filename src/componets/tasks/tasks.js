209940c9365880567f8033ef1b01af160c963eef
Pavel Ksenzhik <ksyonzhik2000@mail.ru>
2022-06-14 23:27:36 +0300

    Add search query to url
    ok

--- src/componets/tasks/tasks.js
+++ src/componets/tasks/tasks.js
@@ -4,33 +4,15 @@ import DoneTasks from './done-tasks';
 
 import './tasks.css'
 
-import { useSearchParams } from 'react-router-dom';
 
 function Tasks() {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const todayDate = Date.now();
-    const filterCondition = {
-        startDate: new Date(todayDate).setDate(new Date(todayDate).getDate() - 1),
-        endDate: 5555555555555555555,
-    }
 
-    const dateQuery = searchParams.get('date') || '';
-    switch(dateQuery){
-        case 'tomorrow': 
-            filterCondition.endDate =  new Date(todayDate).setDate(new Date(todayDate).getDate() + 2)
-            break;
-        case 'week':
-            filterCondition.endDate = new Date(todayDate).setDate(new Date(todayDate).getDate() + 7);
-            break;
-    }
-    console.log(dateQuery);
-    console.log(filterCondition);
     return (
         <div className='tasks'>
             <TaskForm />
             <div className='tasks__wrapper'>
-                <ActiveTasks filterCondition={filterCondition}/>
-                <DoneTasks filterCondition={filterCondition}/>
+                <ActiveTasks />
+                <DoneTasks />
             </div>
         </div>
     )
